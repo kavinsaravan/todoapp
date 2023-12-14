0bdb2e21506405c8a7f514f388fa150ef7d463d2
Kavin <kavinsaravan@gmail.com>
2023-12-14 13:46:56 -0800

    Initial Commit

---
+++ bruhjasdvf/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  bruhjasdvf
+//
+//  Created by Kavin Saravanan on 12/14/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ bruhjasdvf/bruhjasdvfApp.swift
@@ -0,0 +1,17 @@
+//
+//  bruhjasdvfApp.swift
+//  bruhjasdvf
+//
+//  Created by Kavin Saravanan on 12/14/23.
+//
+
+import SwiftUI
+
+@main
+struct bruhjasdvfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
