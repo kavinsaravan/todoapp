bea60b71440c07a9cf7ea41b355de3096243bb13
Kavin <kavinsaravan@gmail.com>
2023-12-16 17:52:51 -0800

    tried to do stuff

--- bruhjasdvf/ContentView.swift
+++ bruhjasdvf/ContentView.swift
@@ -7,6 +7,11 @@
 
 import SwiftUI
 
+struct TodoItem: Identifiable {
+    let id = UUID()
+    let todo: String
+}
+
 struct ContentView: View {
     
     @State private var newTodo = ""
@@ -14,34 +19,55 @@ struct ContentView: View {
     
     var body: some View {
         NavigationView{
-            VStack {
+            VStack() {
                 HStack {
                     TextField("Add todo...", text: $newTodo).textFieldStyle(RoundedBorderTextFieldStyle())
                     Button(action:  {
                         guard !self.newTodo.isEmpty else { return }
                         self.allTodos.append(TodoItem(todo: self.newTodo))
                         self.newTodo = "" 
+                        self.saveTodos()
                     }) {
                         Image(systemName: "plus")
                     }
+                    /*Button(action:  {
+                        guard !self.newTodo.isEmpty else { return }
+                        self.allTodos.append(TodoItem(todo: self.newTodo))
+                        self.newTodo = ""
+                    }) {
+                        Image(systemName: "minus")
+                    }*/
                     .padding(.leading, 5)
                 }.padding()
                 List {
                     ForEach(allTodos) { todoItem in
                         Text(todoItem.todo)
-                        
-                    }
+                    }.onDelete(perform: deleteTodo)
                 }
             }
-     .navigationTitle("Todos")
+            .navigationTitle("Todos")
+        }.onAppear(perform: loadTodos)
     }
-  }
+    
+    private func saveTodos() {
+        UserDefaults.standard.set(try? PropertyListEncoder().encode(self.allTodos), forKey: "todosKey")
+    }
+    
+    private func loadTodos() {
+        if let todosData = UserDefaults.standard.value(forKey: "todosKey") as? Data {
+            if let todosList = try? PropertyListDecoder().decode(Array<TodoItem>.self, from: todosData) {
+                self.allTodos = todosList
+            }
+        }
+    }
+    
+    private func deleteTodo(at offsets: IndexSet) {
+        self.allTodos.remove(atOffsets: offsets)
+        saveTodos()
+    }
 }
     
-    struct TodoItem: Identifiable {
-        let id = UUID()
-        let todo: String
-    }
+    
 
 #Preview {
     ContentView()
